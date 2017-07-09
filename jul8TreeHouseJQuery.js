89e7bd6d543934c0f6a40cd275958a7cab777ebb
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-08 18:32:11 -0700

    Treehouse New JQuery Basics

--- jul8TreeHouseJQuery.js
+++ jul8TreeHouseJQuery.js
@@ -31,3 +31,37 @@ var students = [{
 students.forEach(function(student) {
   console.log(student.name);
 });
+
+//Hide the warning
+//Then show the warning slowly
+
+//$(document).ready(function() {
+//  $('.warning').hide();
+//});
+
+jQuery('.warning').hide();
+jQuery('.warning').show("slow");
+//DRY
+$('.warning').hide().show("slow");
+
+
+//Prevent Spoiler from seeing spoilers
+//Solution, hide spoilers and reveal them through user interaction.
+//1. Hide spoiler
+$('span').hide();
+//2. Add a button
+$('.spoiler').append('<button>Reveal Spoiler</button>');
+//3.When button is pressed show spoiler
+$('button').click(function() {
+  $(this).prev().show();
+  $(this).remove();
+});
+
+
+$('body').append('<div id="overlay"></div>');
+
+$('#imageGallery a').click(function(event) {
+  event.preventDefault();
+  var href = $(this).attr('href');
+  console.log(href);
+});
