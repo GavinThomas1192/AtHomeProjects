5845312d9543cb9ba7c498991532bed1bd75018c
Gavin Thomas <gthomas1192@gmail.com>
2017-08-09 19:17:34 -0700

    still learning

--- chai_mocha_treehouse/textUtilites.js
+++ chai_mocha_treehouse/textUtilites.js
@@ -20,3 +20,21 @@ expect(titleCase('a')).to.equal('A');
 expect(titleCase('vertigo')).to.equal('Vertigo');
 
 expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
+
+
+//Some stuff pulled from internet for github upload
+"use strict";
+
+var expect = require('chai').expect;
+
+// Simple assertions
+expect({}).to.exist;
+expect(26).to.equal(26);
+expect(false).to.be.false;
+expect('hello').to.be.string;
+
+// Modifiers ('not')
+expect([1, 2, 3]).to.not.be.empty;
+
+// Complex chains
+expect([1, 2, 3]).to.have.length.of.at.least(3);
